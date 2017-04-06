0a272eecb59a6435e9e9b78b88d5f93432943465
Jakob Backlund <jakob.backlund@ecraft.com>
2017-04-06 08:44:44 +0300

    Added comments. Removed copy paste code.

--- spec/ecraft/extensions/date.rb
+++ spec/ecraft/extensions/date.rb
@@ -1,12 +1,10 @@
-require 'json'
-
+require 'json/add/core'
 require 'ecraft/extensions/date'
 
 describe Date do
   describe '#to_json' do
-    it 'should serialize integral decimals without decimals' do
+    it 'should serialize the date in yyyy-mm-dd form' do
       json = { 'date' => Date.new(2000, 1, 1) }.to_json
-      puts json
       expect(json).to eq '{"date":"2000-01-01"}'
     end
   end
