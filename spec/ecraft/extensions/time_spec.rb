37a58c1f10908947e56142097ce7d52efac7f927
Per Lundberg <per.lundberg@ecraft.com>
2017-10-31 12:40:09 +0200

    rspec: Use context
    Also fixed a bug; we were describing _Date_ when in fact the tests were testing the _Time_ class...

--- spec/ecraft/extensions/time_spec.rb
+++ spec/ecraft/extensions/time_spec.rb
@@ -1,20 +1,22 @@
 require 'json'
 require 'ecraft/extensions/time'
 
-describe Date do
+describe Time do
   # More details: https://en.wikipedia.org/wiki/ISO_8601
   describe '#to_json' do
-    context 'for values without a fraction part' do
-      it 'serializes the date and time in ISO 8601 form, not including a fraction part' do
-        json = { 'time' => Time.new(2000, 1, 1, 0, 0, 0, '+02:00') }.to_json
-        expect(json).to eq '{"time":"2000-01-01T00:00:00+02:00"}'
+    context 'serializes the date and time in ISO 8601 form' do
+      context 'with a fraction part' do
+        it 'renders the milliseconds part' do
+          json = { 'time' => Time.new(2000, 1, 1, 1, 2, 3.399, '+02:00') }.to_json
+          expect(json).to eq '{"time":"2000-01-01T01:02:03.399+02:00"}'
+        end
       end
-    end
 
-    context 'for values with a fraction part' do
-      it 'serializes the date and time in ISO 8601 form, including the fraction part' do
-        json = { 'time' => Time.new(2000, 1, 1, 1, 2, 3.399, '+02:00') }.to_json
-        expect(json).to eq '{"time":"2000-01-01T01:02:03.399+02:00"}'
+      context 'without a fraction part' do
+        it 'renders no milliseconds' do
+          json = { 'time' => Time.new(2000, 1, 1, 0, 0, 0, '+02:00') }.to_json
+          expect(json).to eq '{"time":"2000-01-01T00:00:00+02:00"}'
+        end
       end
     end
   end
