38055a78f63b3de60c17a2cb0e700affaf349280
Per Lundberg <per.lundberg@ecraft.com>
2017-10-31 12:17:38 +0200

    Fixed spec failure
    The code was stupid; it made the blind assumption that it was being run in the +02:00 time zone...

--- spec/ecraft/extensions/time_spec.rb
+++ spec/ecraft/extensions/time_spec.rb
@@ -6,14 +6,14 @@ describe Date do
   describe '#to_json' do
     context 'for values without a fraction part' do
       it 'serializes the date and time in ISO 8601 form, not including a fraction part' do
-        json = { 'time' => Time.new(2000, 1, 1) }.to_json
+        json = { 'time' => Time.new(2000, 1, 1, 0, 0, 0, '+02:00') }.to_json
         expect(json).to eq '{"time":"2000-01-01T00:00:00+02:00"}'
       end
     end
 
     context 'for values with a fraction part' do
       it 'serializes the date and time in ISO 8601 form, including the fraction part' do
-        json = { 'time' => Time.new(2000, 1, 1, 1, 2, 3.399) }.to_json
+        json = { 'time' => Time.new(2000, 1, 1, 1, 2, 3.399, '+02:00') }.to_json
         expect(json).to eq '{"time":"2000-01-01T01:02:03.399+02:00"}'
       end
     end
@@ -21,7 +21,7 @@ describe Date do
 
   describe '#as_ms' do
     it 'should convert time to ms' do
-      ms = Time.new(2000, 1, 1).as_ms
+      ms = Time.new(2000, 1, 1, 0, 0, 0, '+02:00').as_ms
       expect(ms).to eq 946_677_600_000
     end
   end
