319ae7fdb8a0288eafdc23ab27af1c3d62cdcf21
Per Lundberg <per.lundberg@ecraft.com>
2018-03-06 14:57:02 +0200

    :cop: Comply with Rubocop 0.53 rules

--- lib/ecraft/extensions/ostruct.rb
+++ lib/ecraft/extensions/ostruct.rb
@@ -1,7 +1,7 @@
 require 'ostruct'
 
 class ::OpenStruct
-  def to_json(*_a)
+  def to_json(*_args)
     marshal_dump.to_json
   end
 end
--- lib/ecraft/extensions/time.rb
+++ lib/ecraft/extensions/time.rb
@@ -6,7 +6,7 @@ class ::Time
     to_i * 1000 + usec / 1000
   end
 
-  def to_json(*_a)
+  def to_json(*_args)
     # Use the precision provided in our receiver, but only up to milliseconds at max. Objects lacking the fraction part
     # altogether will be serialized without fraction part using this algorithm.
     fraction_digits = [nsec.to_s.sub(/0+$/, '').length, 3].min
